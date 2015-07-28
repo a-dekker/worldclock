d68e6b586e98f3809f2676262e3f58d353c51fe3
Åke Engelbrektson <eson57@users.noreply.github.com>
2015-07-28 08:02:18 +0200

    Create harbour-worldclock-sv.ts
    Swedish translation if wanted.

---
+++ translations/harbour-worldclock-sv.ts
@@ -0,0 +1,223 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="sv">
+<context>
+    <name>About</name>
+    <message>
+        <location filename="../qml/pages/About.qml" line="19"/>
+        <source>About</source>
+        <translation>Om</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/About.qml" line="22"/>
+        <source>Info</source>
+        <translation>Info</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/About.qml" line="54"/>
+        <source>Version</source>
+        <translation>Version</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/About.qml" line="59"/>
+        <source>City times across the world</source>
+        <translation>Stadstider världen runt</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/About.qml" line="65"/>
+        <source>Author</source>
+        <translation>Utvecklare</translation>
+    </message>
+</context>
+<context>
+    <name>Aliases</name>
+    <message>
+        <location filename="../qml/pages/Aliases.qml" line="38"/>
+        <source>Save</source>
+        <translation>Spara</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Aliases.qml" line="39"/>
+        <source>Cancel</source>
+        <translation>Avbryt</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Aliases.qml" line="78"/>
+        <source>Deleting</source>
+        <translation>Tar bort</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Aliases.qml" line="119"/>
+        <source>Alt. name</source>
+        <translation>Alt. namn</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Aliases.qml" line="140"/>
+        <source>No custom city names defined. Press the plus button to add one.</source>
+        <translation>Inga anpassade namn definierade. Tryck på plusknappen för att lägga till ett.</translation>
+    </message>
+</context>
+<context>
+    <name>CityDetail</name>
+    <message>
+        <location filename="../qml/pages/CityDetail.qml" line="65"/>
+        <source>Timezone details</source>
+        <translation>Tidszondetaljer</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/CityDetail.qml" line="80"/>
+        <source>Timezone</source>
+        <translation>Tidszon</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/CityDetail.qml" line="90"/>
+        <source>Zone time</source>
+        <translation>Zontid</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/CityDetail.qml" line="100"/>
+        <source>Local time</source>
+        <translation>Lokal tid</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/CityDetail.qml" line="110"/>
+        <source>Time difference</source>
+        <translation>Tidsskillnad</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/CityDetail.qml" line="116"/>
+        <source> hour</source>
+        <translation>tim.</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/CityDetail.qml" line="120"/>
+        <source>Prev. daylight transition </source>
+        <translation>Föreg. dagsljusanpassn. </translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/CityDetail.qml" line="136"/>
+        <source>Next daylight transition </source>
+        <translation>Nästa dagsljusanpassn. </translation>
+    </message>
+</context>
+<context>
+    <name>MainPage</name>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="305"/>
+        <source>About</source>
+        <translation>Om</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="309"/>
+        <source>Settings</source>
+        <translation>Inställningar</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="313"/>
+        <source>Custom city names</source>
+        <translation>Anpassade stadsnamn</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="317"/>
+        <source>Add city</source>
+        <translation>Lägg till stad</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="462"/>
+        <source>Details</source>
+        <translation>Detaljerat</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="478"/>
+        <source>Remove</source>
+        <translation>Ta bort</translation>
+    </message>
+</context>
+<context>
+    <name>SettingPage</name>
+    <message>
+        <location filename="../qml/pages/SettingPage.qml" line="48"/>
+        <source>Save</source>
+        <translation>Spara</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SettingPage.qml" line="49"/>
+        <source>Cancel</source>
+        <translation>Avbryt</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SettingPage.qml" line="52"/>
+        <source>Settings</source>
+        <translation>Inställningar</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SettingPage.qml" line="58"/>
+        <source>Sort order</source>
+        <translation>Sortering</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SettingPage.qml" line="59"/>
+        <source>Sort order of personal list</source>
+        <translation>Sorteringsordning för personlig lista</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SettingPage.qml" line="63"/>
+        <source>None</source>
+        <translation>Ingen</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SettingPage.qml" line="66"/>
+        <source>TimeZone</source>
+        <translation>Tidszon</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SettingPage.qml" line="69"/>
+        <location filename="../qml/pages/SettingPage.qml" line="87"/>
+        <source>City</source>
+        <translation>Stad</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SettingPage.qml" line="76"/>
+        <source>Sort order list</source>
+        <translation>Sortering</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SettingPage.qml" line="77"/>
+        <source>Sort order of complete citylist</source>
+        <translation>Sortering för fullständig stadslista</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SettingPage.qml" line="84"/>
+        <source>TimeZone/Continent/City</source>
+        <translation>Tidszon/Kontinent/Stad</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SettingPage.qml" line="90"/>
+        <source>Country</source>
+        <translation>Land</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SettingPage.qml" line="98"/>
+        <source>Hide if current</source>
+        <translation>Dölj om aktuell</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SettingPage.qml" line="99"/>
+        <source>Hide localtime if city is present and current</source>
+        <translation>Dölj lokal tid om staden är aktuell</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SettingPage.qml" line="81"/>
+        <source>Continent/City</source>
+        <translation>Kontinent/stad</translation>
+    </message>
+</context>
+<context>
+    <name>Timezone</name>
+    <message>
+        <location filename="../qml/pages/Timezone.qml" line="39"/>
+        <source>Select city</source>
+        <translation>Välj stad</translation>
+    </message>
+</context>
+</TS>
